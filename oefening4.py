cd8c064e13500c9907f6752515f958d992c861e2
Nuri De Backer <nuri1@telenet.be>
2025-03-30 18:05:12 +0200

    a

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,12 @@
-def countTargetPairs():
+def countTargetPairs(lijst, getal):
+    n = len(lijst)
+    aantal = 0  
+    for i in range(n):
+        for j in range(i + 1, n):
+            if lijst[i] + lijst[j] < getal:
+                aantal += 1
+    return aantal
+
+nums = [-6,2,5,-2,-7,-1,3]
+target = -2
+print(countTargetPairs(nums, target))  
